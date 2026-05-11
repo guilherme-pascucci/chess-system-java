5ff53ff50dcbdb02ca06db1421bc70c2b0e56b61
Guilherme Pascucci Coelho da Cruz <guilherme.cruz@sistran.com.br>
2026-05-11 14:43:48 -0300

    Placing pieces on the board

---
+++ src/King.java
@@ -0,0 +1,11 @@
+public class King extends ChessPiece{
+
+    public King(Board board, Color color) {
+        super(board, color);
+    }
+
+    @Override
+    public String toString() {
+        return "K";
+    }
+}
---
+++ src/Rook.java
@@ -0,0 +1,11 @@
+public class Rook extends ChessPiece{
+
+    public Rook(Board board, Color color) {
+        super(board, color);
+    }
+
+    @Override
+    public String toString() {
+        return "R";
+    }
+}
