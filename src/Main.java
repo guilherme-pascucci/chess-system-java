0ada7a347c0f5ffd62114e1f933f34e4a23297db
Guilherme Pascucci Coelho da Cruz <guilherme.cruz@sistran.com.br>
2026-05-18 14:19:10 -0300

    Check logic

--- src/Main.java
+++ src/Main.java
@@ -33,6 +33,7 @@ public class Main{
                     captured.add(capturedPiece);
                 }
             }
+
             catch (ChessException e){
                 System.out.println(e.getMessage());
                 sc.nextLine();
